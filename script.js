e37d1c0cc31a849f2eaba14caf384d6564cc5c0c
homem do acre <zecarminelli@gmail.com>
2023-06-09 19:27:20 -0300

    Validação do formulário e do email + ajustes no css

--- script.js
+++ script.js
@@ -35,39 +35,41 @@ function CustomAlert() {
 let customAlert = new CustomAlert();
 
 function CustomAlertWait() {
-    this.alert = function (message, title) {
-      document.body.innerHTML =
-        document.body.innerHTML +
-        '<div id="overlay" class="animado"></div><div id="dialogbox" class="slit-in-vertical"><div><div id="alertHeader"></div><div id="alertBody"></div><div id="alertFooter"></div></div></div>';
-  
-      let overlay = document.getElementById("overlay");
-      let dialogbox = document.getElementById("dialogbox");
-  
-      let winH = window.innerHeight;
-      overlay.style.height = winH + "px";
-  
-      overlay.style.display = "block";
-      dialogbox.style.display = "block";
-  
-      document.getElementById("alertHeader").style.display = "block";
-  
-      if (typeof title === "undefined") {
-        document.getElementById("alertHeader").style.display = "none";
-      } else {
-        document.getElementById("alertHeader").innerHTML =
-          '<i class="fa fa-exclamation-circle" aria-hidden="true"></i> ' + title;
-      }
-      document.getElementById("alertFooter").innerHTML = '';
-      document.getElementById("alertBody").innerHTML = message + '<p class="dot one">.</p><p class="dot two">.</p><p class="dot three">.</p>';
-    };
-  
-    this.ok = function () {
-      document.getElementById("dialogbox").style.display = "none";
-      document.getElementById("overlay").style.display = "none";
-    };
-  }
+  this.alert = function (message, title) {
+    document.body.innerHTML =
+      document.body.innerHTML +
+      '<div id="overlay" class="animado"></div><div id="dialogbox" class="slit-in-vertical"><div><div id="alertHeader"></div><div id="alertBody"></div><div id="alertFooter"></div></div></div>';
 
-  let customAlertWait = new CustomAlertWait();
+    let overlay = document.getElementById("overlay");
+    let dialogbox = document.getElementById("dialogbox");
+
+    let winH = window.innerHeight;
+    overlay.style.height = winH + "px";
+
+    overlay.style.display = "block";
+    dialogbox.style.display = "block";
+
+    document.getElementById("alertHeader").style.display = "block";
+
+    if (typeof title === "undefined") {
+      document.getElementById("alertHeader").style.display = "none";
+    } else {
+      document.getElementById("alertHeader").innerHTML =
+        '<i class="fa fa-exclamation-circle" aria-hidden="true"></i> ' + title;
+    }
+    document.getElementById("alertFooter").innerHTML = "";
+    document.getElementById("alertBody").innerHTML =
+      message +
+      '<p class="dot one">.</p><p class="dot two">.</p><p class="dot three">.</p>';
+  };
+
+  this.ok = function () {
+    document.getElementById("dialogbox").style.display = "none";
+    document.getElementById("overlay").style.display = "none";
+  };
+}
+
+let customAlertWait = new CustomAlertWait();
 
 function mandarEmail() {
   var params = {
@@ -75,20 +77,27 @@ function mandarEmail() {
     email: document.getElementById("email").value,
     mensagem: document.getElementById("message").value,
   };
+  const nomev = document.getElementById("name").value;
+  const emailv = document.getElementById("email").value;
+  const mensagemv = document.getElementById("message").value;
+  if (nomev == "" || emailv == "" || mensagemv == "") {  // se tiver vazio
+    customAlert.alert("Todos os campos devem ser preenchidos", "Erro");
+  } else if (!emailv.includes("@") || !emailv.includes(".")) { //se n for um email
+    customAlert.alert("O email precisa ser válido", "Erro");
+  } else {
+    customAlertWait.alert("Estamos enviando seu email", "Aguarde");
+    setTimeout(() => {
+      customAlert.alert("Seu email foi enviado", "Sucesso!");
+    }, 4000);
 
-  customAlertWait.alert("Estamos enviando seu email", "Aguarde");
-  setTimeout(() => {customAlert.alert("Seu email foi enviado", "Sucesso!");},4000);
-  
+    const serviceID = "service_mcdvzxc";
+    const templateID = "template_ghy15go";
 
-  const serviceID = "service_mcdvzxc";
-  const templateID = "template_ghy15go";
-
-  emailjs
-  .send(serviceID, templateID, params)
-  .then((res) => {
-    document.getElementById("name").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("message").value = "";
-    console.log(res);
-  });
+    /*emailjs.send(serviceID, templateID, params).then((res) => {
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
+      console.log(res);
+    });*/
+  }
 }
